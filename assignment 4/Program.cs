b336b2a9d2fc02f9049fa6de4984de06b4ba7851
esl3 <esl3@ESL3>
2024-12-26 16:27:00 +0200

    Add project files.

---
+++ assignment 4/Program.cs
@@ -0,0 +1,255 @@
+﻿namespace assignment_4
+{
+    //classes --> functions
+    //struct  --> functions
+    //interface --> functions
+    //enum --> functions
+    internal class Demo
+    {
+        #region functions and parameters
+        //static void PrintShapes()
+        //{
+        //    for (int i = 1; i <= 10; i++)
+        //    {
+        //        Console.WriteLine("*_*");
+        //    }
+        //}
+
+        //static void PrintShapes(int Count)
+        //{
+        //    for (int i = 1; i <=Count; i++)
+        //    {
+        //        Console.WriteLine("*_*");
+        //    }
+        //}
+        //static void PrintShapes(string pattern,int count=4)
+        //{
+        //    for (int i = 1; i <= count; i++)
+        //    {
+        //        Console.WriteLine(pattern);
+        //    }
+        //}
+        //static int SumNumber(int Number1,int Number2)
+        //  {
+        //      return Number1 + Number2;
+        //  }
+        //static void Swap(ref int x, ref int y)
+        //{
+        //    int temp = x;
+        //    x = y;
+        //    y = temp;
+
+        //} 
+        #endregion
+        //public static int SumArray(int[] arr)
+        //{
+        //    int sum = 0;
+        //    arr[0] = 100;
+        //    for(int i=0;i<arr.Length;i++)
+        //    {
+        //        sum += arr[i];
+        //    }
+        //    return sum;
+        //}
+
+        //public static int SumArray(ref int[] arr)
+        //{
+        //    int sum = 0;
+        //    arr[0] = 100;
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        sum += arr[i];
+        //    }
+        //    return sum;
+        //}
+        //public static int SumArray(ref int[] arr)
+        //{
+        //    int sum = 0;
+        //    arr = new int[] { 4, 5, 6 };
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        sum += arr[i];
+        //    }
+        //    return sum;
+        //}
+
+
+
+        static void Main()
+        {
+            #region 1D Array
+            //int[] Numbers;
+            //declare for reference from type 'Array Of Integers'
+            //Numbers : can refer to object from type 'Array Of Integers'
+            //Numbers : refer to null
+
+            //Numbers = new int[5];
+            //allocated the numbers of required bytes for the object at Heap(4 Bytes*5)
+            //initialized the alocated of bytes with default value of the datatypes
+            //user defind constructor of exisits
+            //assign the object to referene Numbers
+            //Numbers[0] = 12;
+            //Numbers[1] = 6;
+            //Numbers[2] = 7;
+            //Numbers[3] = 11;
+            //Numbers[4] = 19;
+            //Numbers[5] = 15; out of range
+            //Console.WriteLine(Numbers[0]);
+            //Console.WriteLine(Numbers[1]);
+            //Console.WriteLine(Numbers[2]);
+            //Console.WriteLine(Numbers[3]);
+            //Console.WriteLine(Numbers[4]);
+            //int[] Number01 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //int[] Number02 = new int[5] { 1, 2, 3, 4, 5 };
+            //for(int i = 0; i < Numbers.Length; i++)
+            //{
+            //    Console.Write($"Enter Numbers [{i}]: ");
+            //    Numbers[i]=int.Parse( Console.ReadLine() );
+            //}
+
+            //Console.WriteLine("==================================");
+            //for(int i=0; i< Numbers.Length; i++)
+            //{
+            //    Console.WriteLine($"{Numbers[i]}");
+            //} 
+            #endregion
+
+            #region 2D Array
+            //2D Array
+            //int[,] Marks = new int[3,3];
+            //Marks[0, 0] = 99;
+            //Marks[0, 1] = 99;
+            //Marks[0, 2] = 99;
+
+            //Marks[1, 0] = 63;
+            //Marks[1, 1] = 100;
+            //Marks[1, 2] = 70;
+
+            //Marks[2, 2] = 40;
+            //Marks[2, 0] = 50;
+            //Marks[2, 1] = 93;
+
+            //Console.WriteLine(Marks[0, 0]);
+            //Console.WriteLine(Marks[0, 1]);
+            //Console.WriteLine(Marks[0, 2]);
+
+            //Console.WriteLine(Marks[1, 0]);
+            //Console.WriteLine(Marks[1, 1]);
+            //Console.WriteLine(Marks[1, 2]);
+
+            //Console.WriteLine(Marks[2, 0]);
+            //Console.WriteLine(Marks[2, 1]);
+            //Console.WriteLine(Marks[2, 2]);
+
+
+            //for(int i = 0; i <3; i++)
+            //{
+            //    for(int j = 0; j < 3; j++)
+            //    {
+            //        Marks[i,j]=int.Parse(Console.ReadLine());
+            //    }
+            //}
+
+            //Console.WriteLine("=============================");
+
+            //for(int i = 0;i < 3; i++)
+            //{
+            //    for (int j = 0;j < 3; j++)
+            //    {
+            //        Console.Write($"{Marks[i, j]} "  );
+            //    }
+            //    Console.WriteLine();
+            //} 
+            #endregion
+
+            #region juddge Array
+            // juddge Array
+            //int[][] Marks = new int[3][];
+            //Marks[0] = new int [] { 1, 2, 3 };
+            //Marks[1] = new int[] { 1, 2 };
+            //Marks[1] = new int[] { 1, 2 , 3 ,4 };
+
+            //Marks[0]=new int[3];
+            //Marks[1]=new int[5];
+            //Marks[2]=new int[2];
+            // for(int i = 0; i < 3; i++)
+            // {
+            //    for(int j = 0; j < Marks[i].Length; j++)
+            //    {
+            //        Marks[i][j] =int.Parse(Console.ReadLine());
+            //    }
+            // }
+            //Console.WriteLine("===================================");
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < Marks[i].Length; j++)
+            //    {
+            //        Console.Write($"{Marks[i][j]} ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Functions
+            //Functions : block of code that have name if need to excute this code
+            //you need call function by the name 
+            //function prototype
+            //1.signature
+            //1.1name 
+            //1.2Return Value
+            //1.3parameters
+            //2.Body(code)
+
+            //PrintShapes(5,"****"); passing by order
+            //PrintShapes(pattern: "%_%", count: 6); passing by name
+            //PrintShapes("$#*$");
+            //Console.WriteLine(SumNumber(5, 4)); 
+            #endregion
+
+            #region pass by value and ref in value type
+            //int A=4,B=5;
+            //Console.WriteLine($"A: {A}");
+            //Console.WriteLine($"B: {B}");
+            //Console.WriteLine("After Swap");
+            //Swap(A,B);
+            //Console.WriteLine($"A: {A}");
+            //Console.WriteLine($"B: {B}");
+
+            //int A = 4, B = 5;
+            //Console.WriteLine($"A: {A}");
+            //Console.WriteLine($"B: {B}");
+            //Console.WriteLine("After Swap");
+            //Swap(ref A,ref B);
+            //Console.WriteLine($"A: {A}");
+            //Console.WriteLine($"B: {B}");
+
+            #endregion
+
+            #region Pass by value and ref in reference type
+            //pass ref type
+            //1.passing by value
+            //int[] Numbers = { 1, 2, 3 };
+            //Console.WriteLine(SumArray(Numbers));
+
+            //2.passing by reference
+            //int[] Numbers = { 1, 2, 3 };
+            //Console.WriteLine(SumArray(ref Numbers)); 
+            #endregion
+
+            //passing Parameter refernce type Ex02
+            //by value
+            //int[] Numbers = { 1, 2, 3 };
+            //Console.WriteLine(SumArray(Numbers));
+            //Console.WriteLine(Numbers[0]);
+
+            //by ref
+            //int[] Numbers = { 1, 2, 3 };
+            //Console.WriteLine(SumArray(ref Numbers));
+            //Console.WriteLine(Numbers[0]);
+
+
+        }
+    }
+}
---
+++ assignment4 solution/Program.cs
@@ -0,0 +1,260 @@
+﻿using System.Collections.Specialized;
+
+namespace assignment4_solution
+{
+    internal class Program
+    {
+        #region passing by value Example
+        //static void Swap( int x, int y)
+        //{
+        //    int temp = x;
+        //    x = y;
+        //    y = temp;
+
+        //} 
+        #endregion
+
+        #region pass by ref Example
+        //static void Swap(ref int x, ref int y)
+        //{
+        //    int temp = x;
+        //    x = y;
+        //    y = temp;
+
+        //} 
+        #endregion
+
+        #region pass by reference type
+        //public static int SumArray( int[] arr)
+        //{
+        //    int sum = 0;
+        //    arr = new int[] { 14, 51, 61 };
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        sum += arr[i];
+        //    }
+        //    return sum;
+        //}
+
+        //public static int SumArray(ref int[] arr)
+        //{
+        //    int sum = 0;
+        //    arr = new int[] { 14, 51, 61 };
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        sum += arr[i];
+        //    }
+        //    return sum;
+        //}
+
+        #endregion
+
+        #region Q3
+        //static  void SumAndSubstraction(int Number1, int Number2,out int Sum,out int Sub) 
+        //  {
+        //      Console.WriteLine("Enter Number1 : ");
+        //      Number1=int.Parse(Console.ReadLine());
+        //      Console.WriteLine("Enter Number2 : ");
+        //      Number2 =int.Parse(Console.ReadLine());
+
+        //      Sum=Number1+ Number2;
+        //      Sub = Number1 - Number2;
+        //      Console.Write($"The Sum Is: {Sum}" );
+        //      Console.WriteLine();
+        //      Console.Write($"The Substruction Is: {Sub}");
+        //  } 
+        #endregion
+
+        #region Q4
+        //Q4
+        //static int Check_individual()
+        //{
+        //    Console.Write("Enter Number: ");
+        //    int Number= int.Parse(Console.ReadLine());
+        //    if (Number % 2 == 0)
+        //        return 0;
+        //    else
+        //        return Number;
+        //}
+        //static void Calculate_Sum_Individual(int Number)
+        //{
+        //    int Sum = 0;
+        //    int Reaminder = 0;
+        //    for(int i = 0; i <= Number; i++)
+        //    {
+        //        Reaminder = Number % 10;
+        //        Number/=10;
+        //        Sum += Reaminder;
+
+
+        //    }
+        //    Console.WriteLine($"The sum of the digits of the number {Reaminder} is {Sum}");
+
+        //} 
+        #endregion
+
+        #region Q5
+        //Q5
+        //static bool Is_Prime(int number)
+        //{
+        //    Console.Write("Enter Number: ");
+        //    number=int.Parse(Console.ReadLine());
+        //    if (number == 2)
+        //        return true;
+        //    else if (number < 2 || number % 2 == 0)
+        //        return false;
+        //    else 
+        //    {
+        //        for(int i = 3; i <= number / 2; i += 2)
+        //        {
+        //            if(number % 1==0)
+        //                return false;
+        //        }
+        //    }
+        //    return true;
+
+        //} 
+        #endregion
+
+        #region Q6
+        ////Q6
+        //static void Check_Minimum_Maxmum(int Number,ref int Min,ref int Max)
+        //{
+        //    int[] arr = new int[5];
+        //    Min = arr[0];
+        //    Max = arr[0];
+        //    Console.WriteLine("Enter Numbers");
+        //    for(int i=0;i<arr.Length;i++)
+        //    {
+        //        Number=int.Parse(Console.ReadLine());
+        //        arr[i] = Number;
+        //        if(arr[i] > Max)
+        //            Max = Number;
+        //        else if(arr[i] < Min)
+        //            Min = Number;
+
+
+        //    }
+        //    Console.WriteLine($"Min: {Min} ");
+        //    Console.WriteLine($"Max: {Max}");
+
+
+        //} 
+        #endregion
+
+        #region Q7
+        //static int Factorial_Number(int number)
+        //{
+        //    int Multiply = 1;
+        //    for(int i=1;i<=number;i++)
+        //    {
+        //        Multiply *= i;
+        //    }
+        //    return Multiply;
+        //} 
+        #endregion
+
+        //Q8
+        static string Read_string()
+        {
+            string message=Console.ReadLine();
+            return message;
+        }
+        static char Replace_Letter(char[] Letters,string Read)
+        {
+            for (int i = 0; i < Read.Length; i++)
+            {
+                if (Read[0] == 'h')
+                    Letters[0] = 'w';
+            }
+            return Letters[0];
+
+        static void Main()
+        {
+                #region Q1 Passing by value in case value type 
+                //Passing by value in case value type 
+                //passing by value is not affects of parameters and variables
+                //inside the function and it can't change the parameters value 
+                //int A = 4, B = 5;
+                //Console.WriteLine($"A: {A}");
+                //Console.WriteLine($"B: {B}");
+                //Console.WriteLine("-----------After Swap--------------");
+                //Swap( A, B);
+                //Console.WriteLine($"A: {A}");
+                //Console.WriteLine($"B: {B}");
+
+
+                #endregion
+
+                #region Q1 Passing by reference in case value type
+                //Passing by reference in case value type
+                //pass by ref is:- the same momery location is used for both variables
+                //it can affects on the parameters becouse same the address
+
+                //int A = 4, B = 5;
+                //Console.WriteLine($"A: {A}");
+                //Console.WriteLine($"B: {B}");
+                //Console.WriteLine("----------After Swap-------------------");
+                //Swap(ref A, ref B);
+                //Console.WriteLine($"A: {A}");
+                //Console.WriteLine($"B: {B}");
+                #endregion
+
+                #region Q2 Passing by value in case reference type
+                //Passing by value in case reference type
+                //in that case no would difference between pass by val and ref 
+                //but in some cases would difference between their
+                //examples
+                // here would difference 
+                //int[] Numbers = { 11, 8, 13 };
+                //Console.WriteLine(SumArray(Numbers));
+                //Console.WriteLine(Numbers[0]);
+
+                //passing by ref in case reference type
+                //int[] Numbers = { 11, 8, 13 };
+                //Console.WriteLine(SumArray(ref Numbers));
+                //Console.WriteLine(Numbers[0]);
+
+                #endregion
+
+                #region Q3
+                //Q3
+                //int a=0, b=0, c, d;
+
+                //SumAndSubstraction(a, b,out c,out d);
+
+                #endregion
+
+                #region Q4
+                //Q4
+
+                //int Number = Check_individual();
+                //Calculate_Sum_Individual(Number); 
+                #endregion
+
+                #region Q5
+                ////Q5
+                //    int Number = 0;
+                //    Console.WriteLine(Is_Prime(Number)); 
+                #endregion
+
+                #region Q6
+                //int Number = 0,min=0,max=0;
+                //Check_Minimum_Maxmum(Number,ref min,ref max); 
+                #endregion
+
+                #region Q7
+                ////Q7
+                //int Number = int.Parse(Console.ReadLine());
+                //Console.WriteLine(Factorial_Number(Number)); 
+                #endregion
+
+                #region Q8
+                string message=Read_string();
+                char[] Letters=;
+                Replace_Letter(Letters, message);
+                #endregion
+
+            }
+    }
+}
